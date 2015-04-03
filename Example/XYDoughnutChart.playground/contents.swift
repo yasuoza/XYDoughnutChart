5ca96edbede3b70dd1c88752297bb02b4328949a
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2015-04-03 15:15:56 +0900

    Also upgrade playground for xcode-6.3

---
+++ Example/XYDoughnutChart.playground/contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+import XYDoughnutChart
+
+/*:
+
+Mimimum data sourcing
+
+*/
+
+class DataSource: NSObject, XYDoughnutChartDataSource {
+
+    func numberOfSlicesInDoughnutChart(doughnutChart: XYDoughnutChart) -> Int {
+        return 4
+    }
+
+    func doughnutChart(doughnutChart: XYDoughnutChart, valueForSliceAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return CGFloat(indexPath.slice + 1) * 10
+    }
+
+}
+
+/*:
+
+Draw a chart!
+
+*/
+
+let frame = CGRectMake(0, 0, 150, 150)
+let chartView = XYDoughnutChart(frame: frame)
+let dataSource = DataSource()
+chartView.dataSource = dataSource
+chartView.reloadData()
+
